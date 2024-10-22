b660a56ae56430bf21c44f2adfbbeab4993b1d19
Vinicius Cazuza <cazuza.dev@gmail.com>
2024-10-22 16:29:29 -0300

    deploy: deploy the application to AWS
    - Completed the project setup, including admin validation and vehicle CRUD
    - Configured JWT authentication and Swagger documentation
    - Refactored the project structure and added unit tests for the Admin and Vehicle models
    - Implemented request tests for comprehensive coverage
    - Deployed the application to AWS for production use

--- Api/Infraestrutura/Db/DbContexto.cs
+++ Api/Infraestrutura/Db/DbContexto.cs
@@ -10,11 +10,6 @@ public class DbContexto : DbContext
     {
     }
 
-    public DbContexto(IConfiguration configuracaoAppSettings)
-    {
-        _configuracaoAppSettings = configuracaoAppSettings;
-    }
-    
     public DbSet <Administrador> Administradores { get; set; } = default!;
     public DbSet <Veiculo> Veiculos { get; set; } = default!;
 
